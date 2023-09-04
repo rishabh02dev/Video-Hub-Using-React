dd14e9ab365665dce6178586e4cd2723e22de11f
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-09-04 20:52:04 +0530

    Login Page Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Videos from './components/Videos';
 import Upload from './components/Upload';
+import Login from './components/Login'; 
+import Signup from './components/Signup';
 function App() {
   return (
     <Router>
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/videos" element={<Videos />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </Router>
