c3b461db83de5d763cbcf506d079c66f45cdd670
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-09-04 19:27:31 +0530

    Home Section Completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import { Box, Heading, Image } from '@chakra-ui/react';
 import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Container } from '@chakra-ui/react';
+import { Stack } from '@chakra-ui/react';
 import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
-import React from 'react'
+import React from 'react';
+import { Text } from '@chakra-ui/react';
 
 const headingOptions = {
     pos:"absolute",
@@ -25,6 +27,13 @@ const Home = () => {
    <Container maxW = {'container.xl'} minH = {'100vh'} padding = "16">
     <Heading textTransform={"uppercase"} py ="2" w = "fit-content" borderBottom={'2px solid'}
     m="auto"      >Services</Heading>
+
+   <Stack h = "full" p ={'4'} alignItems={'center'} direction={['column', 'row']} >
+   <Image src={img5} h ={["40", "400"]} filter={'hue-rotate(-130deg)'}/>
+   <Text letterSpacing = {"widest"} lineHeight={"190%"} p ={["4", "16"]} textAlign={"center"} >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero delectus cum ipsam ad, eligendi, pariatur culpa voluptatum tenetur sed deserunt optio repellendus nesciunt, cumque veniam! Labore at quod pariatur velit! Hic explicabo dolores consequuntur praesentium necessitatibus asperiores saepe voluptatibus ipsa, nulla aut corporis ducimus reprehenderit! Ab consectetur obcaecati blanditiis dolore, impedit doloribus odit beatae, unde debitis iure, asperiores error maiores ducimus quaerat iste facilis distinctio soluta! Unde suscipit eveniet alias. Nulla fugit eveniet dolores repellendus blanditiis quibusdam debitis, nesciunt veritatis quas voluptatum tenetur dolor veniam amet, molestiae voluptate ad eaque laboriosam deleniti. Totam dicta nulla magnam nihil nam aliquid quae?
+   </Text>
+   </Stack>
 
    </Container>
    </Box>
