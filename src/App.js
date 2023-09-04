68b10779ca3a9671077b85769ba21843eb7a05a4
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-09-04 20:37:14 +0530

    Upload Section Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Videos from './components/Videos';
+import Upload from './components/Upload';
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/videos" element={<Videos />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
       <Footer />
     </Router>
